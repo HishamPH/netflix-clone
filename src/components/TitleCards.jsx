343ab81393fcaef92fe584347af7c58247c20283
hisham <hishamnarakkal@gmail.com>
2024-12-06 21:37:09 +0530

    authorization bearer token issue fixed

--- src/components/TitleCards.jsx
+++ src/components/TitleCards.jsx
@@ -1,47 +1,53 @@
-import {useEffect, useRef, useState} from 'react'
-import '../styles/TitleCards.css';
-import { Link } from 'react-router-dom';
-import cards_data from '../assets/cards/Cards_data'
-const TitleCards = ({title,category}) => {
-  const [movieData,setMovieData] = useState([]);
+import { useEffect, useRef, useState } from "react";
+import "../styles/TitleCards.css";
+import { Link } from "react-router-dom";
+import cards_data from "../assets/cards/Cards_data";
+const TitleCards = ({ title, category }) => {
+  const [movieData, setMovieData] = useState([]);
   const cardsRef = useRef();
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWY2M2FlNTM5ZDhiZTMwZGYzZDBmODc5MWRlNDc1MSIsInN1YiI6IjY2NjdmZWQ2YzIxY2RlMTE2ZTdjNWI4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kIVjmT0UEa552Q3GBm6_PDZeV53Aifo-cfnCvdyaRTs'
-    }
+      accept: "application/json",
+      Authorization: import.meta.env.VITE_BEARER_TOKEN,
+    },
   };
-  
-  
 
-  const handleWheel = (e)=>{
+  const handleWheel = (e) => {
     e.preventDefault();
     cardsRef.current.scrollLeft += e.deltaY;
-  }
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:'now_playing'}?language=en-US&page=1`, options)
-    .then(response => response.json())
-    .then(response => setMovieData(response.results))
-    .catch(err => console.error(err));
-    cardsRef.current.addEventListener('wheel',handleWheel)
-  },[])
+  };
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setMovieData(response.results))
+      .catch((err) => console.error(err));
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
   return (
-    <div className='titlecards'>
-      <h2>{title?title:'Now Playing'}</h2>
+    <div className="titlecards">
+      <h2>{title ? title : "Now Playing"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {movieData.map((card,index)=>{
+        {movieData.map((card, index) => {
           return (
             <Link to={`/player/${card.id}`} className="card" key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`} alt="" />
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`}
+                alt=""
+              />
               <p>{card.original_title}</p>
             </Link>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,48 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Player.css'
-import back_arrow from '../assets/back_arrow_icon.png'
-import { useParams,Link, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "../styles/Player.css";
+import back_arrow from "../assets/back_arrow_icon.png";
+import { useParams, Link, useNavigate } from "react-router-dom";
 const Player = () => {
   const navigate = useNavigate();
-  const {id} = useParams();
-  const [movieData,setMovieData]=useState({
-    name:'',
-    key:'',
-    published_at:'',
-    type:''
+  const { id } = useParams();
+  const [movieData, setMovieData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
   });
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWY2M2FlNTM5ZDhiZTMwZGYzZDBmODc5MWRlNDc1MSIsInN1YiI6IjY2NjdmZWQ2YzIxY2RlMTE2ZTdjNWI4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kIVjmT0UEa552Q3GBm6_PDZeV53Aifo-cfnCvdyaRTs'
-    }
+      accept: "application/json",
+      Authorization: import.meta.env.VITE_BEARER_TOKEN,
+    },
   };
-  
-  
 
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-    .then(response => response.json())
-    .then(response => setMovieData(response.results[0]))
-    .catch(err => console.error(err));
-  },[])
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setMovieData(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <div className='player'>
+    <div className="player">
+      <img
+        src={back_arrow}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
 
-        <img src={back_arrow} alt="" onClick={()=>{navigate(-2)}}/>
-
-      
-      <iframe src={`https://www.youtube.com/embed/${movieData.key}`} width='90%' height='90%'
-      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <iframe
+        src={`https://www.youtube.com/embed/${movieData.key}`}
+        width="90%"
+        height="90%"
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
       <div className="player-info">
-        <p>{movieData.published_at.slice(0,10)}</p>
+        <p>{movieData.published_at.slice(0, 10)}</p>
         <p>{movieData.name}</p>
         <p>{movieData.type}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
