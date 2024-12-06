624670d681d5023eb783428f16af1434a9b6642f
hisham <hishamnarakkal@gmail.com>
2024-12-06 21:31:17 +0530

    .env file was added and sensitive files were moved

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
-
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyIQy9K_7DsUIq0rVYY11my_dmv6zpCCw",
-  authDomain: "netflix-clone-18b34.firebaseapp.com",
-  projectId: "netflix-clone-18b34",
-  storageBucket: "netflix-clone-18b34.appspot.com",
-  messagingSenderId: "356763263758",
-  appId: "1:356763263758:web:bdcca10e55a295acf8ed5f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_API_KEY,
+  projectId: import.meta.env.VITE_FIREBASE_API_KEY,
+  storageBucket: import.meta.env.VITE_FIREBASE_API_KEY,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_API_KEY,
+  appId: import.meta.env.VITE_FIREBASE_API_KEY,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,35 +21,33 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const signUp = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (e) {
+    console.log(e);
+    alert(e);
+  }
+};
 
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    console.log(e);
+    alert(e);
+  }
+};
 
-const signUp = async(name,email,password)=>{
-    try{
-        const res = await createUserWithEmailAndPassword(auth,email,password);
-        const user = res.user;
-        await addDoc(collection(db,'user'),{
-            uid:user.uid,
-            name,
-            authProvider:'local',
-            email
-        })
-    }catch(e){
-        console.log(e);
-        alert(e);
-    }
-}
+const logout = async () => {
+  signOut(auth);
+};
 
-const login = async(email,password)=>{
-    try{
-        await signInWithEmailAndPassword(auth,email,password)
-    }catch(e){
-        console.log(e);
-        alert(e);
-    }
-}
-
-const logout = async()=>{
-    signOut(auth);
-}
-
-export {auth,db,login,signUp,logout}
+export { auth, db, login, signUp, logout };
